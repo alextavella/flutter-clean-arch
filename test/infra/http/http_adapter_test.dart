6d7d60f81bd0d51f98924bfab15b835379e6a33b
alextavella <eu.alextavella@gmail.com>
2021-03-01 20:29:51 -0300

    ensure HttpAdapter calls post with correct URL

---
+++ test/infra/http/http_adapter_test.dart
@@ -0,0 +1,35 @@
+import 'package:faker/faker.dart';
+import 'package:http/http.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+class HttpClientSpy extends Mock implements Client {}
+
+class HttpAdapter {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  Future<void> request(
+      {required String url, required String method, Map? body}) async {
+    await this.client.post(Uri.parse(url));
+  }
+}
+
+void main() {
+  group('post', () {
+    test('should call POST with correct values', () async {
+      final httpClient = HttpClientSpy();
+      final sut = HttpAdapter(httpClient);
+      final url = faker.internet.httpUrl();
+
+      when(httpClient).calls(#post)
+          .withArgs(positional: [Uri.parse(url)])
+          .thenAnswer((_) async => Response('', 200));
+
+      await sut.request(url: url, method: 'post');
+
+      verify(httpClient).called(#post).once();
+    });
+  });
+}
